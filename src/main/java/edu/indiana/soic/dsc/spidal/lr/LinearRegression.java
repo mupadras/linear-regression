45d4fba8244ab05efea5e189aebbe063b0cc7204
madhu <mupadras@indiana.edu>
2016-06-20 10:43:24 -0400

    Adding initial project for linear regression

---
+++ src/main/java/edu/indiana/soic/dsc/spidal/lr/LinearRegression.java
@@ -0,0 +1,14 @@
+package edu.indiana.soic.dsc.spidal.lr;
+
+import com.google.common.base.Stopwatch;
+
+/**
+ * Created by madhu on 6/20/16.
+ */
+public class LinearRegression {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        Stopwatch mainTimer = Stopwatch.createStarted();
+    }
+}
